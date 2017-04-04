4625e95606304c142736f78545f080c29d97c18f
revanth <rev4nth@gmail.com>
2017-04-04 11:24:02 +0530

    shift to postgres db

--- track_bills/settings.py
+++ track_bills/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'track_bills.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'track_bills',
+            'USER': 'Revanth',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '',
     }
 }
 
